aeb6236543d8ef88a1af8b9b3e1d62fe0cf90ed4
Jridy feriel <jridifer006@gmail.com>
2019-05-31 02:12:09 +0100

    it's about abstraction

---
+++ src/pack/day12.java
@@ -0,0 +1,60 @@
+package pack;
+
+import java.util.*;
+
+abstract class Book {
+    String title;
+    String author;
+
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    abstract void display();
+}
+
+// Declare your class here. Do not use the 'public' access modifier.
+class MyBook extends Book{
+
+    // Declare the price instance variable
+    int price;
+    /**
+     *   Class Constructor
+     *
+     *   @param title The book's title.
+     *   @param author The book's author.
+     *   @param price The book's price.
+     **/
+    // Write your constructor here
+    MyBook(String title,String author,int price){
+        super(title,author);
+        this.price=price;
+    }
+    /**
+     *   Method Name: display
+     *
+     *   Print the title, author, and price in the specified format.
+     **/
+    // Write your method here
+    void display(){
+        System.out.println("Title: "+ super.title);
+        System.out.println("Author: "+ super.author);
+        System.out.println("Price: "+ this.price);
+    }
+}
+// End class
+
+public class day12 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String title = scanner.nextLine();
+        String author = scanner.nextLine();
+        int price = scanner.nextInt();
+        scanner.close();
+
+        Book book = new MyBook(title, author, price);
+        book.display();
+    }
+}
